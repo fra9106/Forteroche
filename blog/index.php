954472c905430cba72ad3f9deac7d0d6ee7a8bc6
Franck Boutot <boutofranck@gmail.com>
2019-09-30 10:34:31 +0200

    premier commit

---
+++ blog/index.php
@@ -0,0 +1,22 @@
+<?php 
+ //routeur
+ require('controller.php');
+
+ if (isset($_GET['action'])) {
+ 	if ($_GET['action'] == 'listPosts') {
+ 		listPosts();
+ 	}
+ 
+ elseif ($_GET['action'] == 'post') {
+ 	if (isset($_GET['id']) && $_GET['id'] = > 0) {
+ 		post();
+ 	}
+ 	else {
+ 		echo ' Error : No post id sent !'
+ 	}
+  } 
+ }
+
+ else {
+ 	listPosts();
+ }
