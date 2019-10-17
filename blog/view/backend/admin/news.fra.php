6864047a54cf6ac222514fefd99b6a50aca19309
fra9106 <55781079+fra9106@users.noreply.github.com>
2019-10-17 15:10:18 +0200

    création blog/view/backend/admin/adminTemplate.php

--- blog/view/backend/admin/news.fra.php
+++ blog/view/backend/admin/news.fra.php
@@ -1,4 +1,6 @@
-<?php 
+<?php $title = 'ChapitreAdmin';?>
+<!--affichage liste des chapitres mis en ligne-->
+<?php ob_start(); 
 session_start();
 try { $bdd = new PDO('mysql:host=localhost;dbname=openclass;charset=utf8', 'root', '');
 
@@ -9,33 +11,10 @@ try { $bdd = new PDO('mysql:host=localhost;dbname=openclass;charset=utf8', 'root
 
 ?>
 
-<html>
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="../../../public/css/style.css">
-  <script src="//cdn.tinymce.com/4/tinymce.min.js"></script> 
-  <script>tinymce.init({ selector:'textarea', content_css : '../../../public/css/style.css', 
-        language_url: 'https://olli-suutari.github.io/tinyMCE-4-translations/fr_FR.js',
-      language: 'fr_FR' });</script>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf " crossorigin="anonymous">
-  
-  <title>chapitreAdmin</title>
-</head>
-<body>
    <div class="vuChapComment">
       <div align="center">
-         <h3>Connecté!</h3>
-         <br/>
-         <br/>
-         <h2>
-            Bonjour Jean !
-
-         </h2>
-         <br />
+         <h3>Connecté!</h3><br/><br/>
+         <h2>Bonjour Jean !</h2><br />
 
          <p>
             Bienvenue chez toi!
@@ -60,7 +39,7 @@ try { $bdd = new PDO('mysql:host=localhost;dbname=openclass;charset=utf8', 'root
   ?>
   <h3>Envoyer un chapitre</h3>
 
-  <form style="margin: 50px;"method="POST" action="">
+  <form class="form"method="POST" action="">
    <input type="text" placeholder="title" id="title" name="title" /><br><br><br>
    <textarea name="content"  rows="5" cols="50" placeholder="Votre message"></textarea><br><br>
    <button type="submit"name="envoi_message"class="btn btn-primary">GO!</button>
@@ -70,7 +49,7 @@ try { $bdd = new PDO('mysql:host=localhost;dbname=openclass;charset=utf8', 'root
 <br />
 
 <a href="../../../menu/blogAccueilConnect.php">Accueil</a>
-<a href="#">Crypto</a>
+
 
 <?php
 
@@ -78,11 +57,8 @@ try { $bdd = new PDO('mysql:host=localhost;dbname=openclass;charset=utf8', 'root
 </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-</body>
-</html>
 <?php
 
 ?>
+<?php $content = ob_get_clean(); ?>
+<?php require('adminTemplate.php'); ?> 
