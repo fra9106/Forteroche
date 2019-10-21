6dfa0264679bc6737d9265a3bd4ef89c9d8c0a8c
fra9106 <55781079+fra9106@users.noreply.github.com>
2019-10-21 14:26:48 +0200

    création fichier controller/backend

---
+++ blog/controller/backend.php
@@ -0,0 +1,34 @@
+<?php 
+//--------Contoller admin
+
+
+require_once('model/postManager.php');// chargement des classes
+require_once('model/commentManager.php');
+
+function editChapitre($title, $content) //fonction affiche chapitre
+{
+	$chapEdit = new PostManager();// création objet postManager
+
+	$chapitre = $chapEdit->postChapitre($title, $content);
+	
+	if($chapitre === false) {
+		die('Je crois que ça va pas être possible d \'ajouter un chapitre...');
+	}else{
+		header('Location: index.php');
+	}
+	
+	 //chargement de la page qui affichera la liste des chapitre
+}
+
+function suppChapitre($postId) 
+{
+	$supprime = new PostManager();
+
+	$deletedPost = $supprime->deletChapitre($postId);
+	
+	if($chapitre === false) {
+		die('Je crois que ça va pas être possible de supprimer un chapitre...');
+	}else{
+		header('Location: index.php');
+	}
+}
